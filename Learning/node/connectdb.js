ecb990c587012f26b9cd6c21e54690d0c92c89ab
DIVYANSHU MISHRA <mishraaman4512@gmail.com>
2025-12-09 18:58:24 +0530

    k d

---
+++ Learning/node/connectdb.js
@@ -0,0 +1,14 @@
+// const express =require("express");
+// const app = express();
+// const mongoose = require("mongoose");
+
+
+// app.listen(3000, async(error)=>{
+//     if(error){
+//         console.log("Error");
+//     }
+//     try{
+//         await mongoose.connect("mongodb://127.0.0.1:27017/testdb") {
+//     }
+    
+// await mongoose.connect("mongodb://127.0.0.1:27017/testdb", {
